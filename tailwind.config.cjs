d4c5194e82a7bbc146621d69ac11697d2f77f280
Lúcio Andrade <lucioandradejr@gmail.com>
2023-09-25 05:08:50 -0400

    feat: dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: ['class', '[data-theme="dark"]'],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				primary: "var(--primary)",
+				muted: {
+					foreground: "var(--muted-foreground)",
+					DEFAULT: "var(--muted-foreground)"
+				}
+			}
+		},
 	},
 	plugins: [],
 }
