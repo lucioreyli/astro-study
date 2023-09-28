bbd8dcc4aafa592abeabbcc7eceb29ffba8e237a
Lúcio Andrade <lucioandradejr@gmail.com>
2023-09-28 10:52:00 -0400

    feat: backing to node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-/* eslint-disable */
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import bun from "astro-bun";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: bun(),
+  adapter: node({ mode: "standalone" }),
 });
