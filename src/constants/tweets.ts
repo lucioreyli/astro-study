7a6966ed08fb05ac97c1afd16d1c3064b05be880
Lúcio Andrade <lucioandradejr@gmail.com>
2023-09-30 09:38:59 -0400

    feat: home page

--- src/constants/tweets.ts
+++ src/constants/tweets.ts
@@ -31,20 +31,20 @@ export const tweets: Tweet[] = [
     replies: [],
     retweets: 0,
   },
-  {
-    id: "1702657504352620942",
-    user: {
-      name: "Sr. Luiz Otávio",
-      user: "SrLuizOtavio",
-      verified: true,
-      photo:
-        "https://pbs.twimg.com/profile_images/1691089842552115201/JQyPbICk_400x400.jpg",
-    },
-    tweet:
-      "Explicação pública: a Poliana, minha namorada atual, não usa calças. Se ela usasse, não seria minha namorada, pois eu não me envolveria com mulher imodesta, que daria mal exemplo às minhas filhas. Minha ex-namorada usava calças, 4 anos atrás; obviamente terminamos. Eis a verdade.",
-    photos: ["F6ENAoAWYAEOMmP"],
-    likes: 0,
-    replies: [],
-    retweets: 0,
-  },
+  // {
+  //   id: "1702657504352620942",
+  //   user: {
+  //     name: "Sr. Luiz Otávio",
+  //     user: "SrLuizOtavio",
+  //     verified: true,
+  //     photo:
+  //       "https://pbs.twimg.com/profile_images/1691089842552115201/JQyPbICk_400x400.jpg",
+  //   },
+  //   tweet:
+  //     "Explicação pública: a Poliana, minha namorada atual, não usa calças. Se ela usasse, não seria minha namorada, pois eu não me envolveria com mulher imodesta, que daria mal exemplo às minhas filhas. Minha ex-namorada usava calças, 4 anos atrás; obviamente terminamos. Eis a verdade.",
+  //   photos: ["F6ENAoAWYAEOMmP"],
+  //   likes: 0,
+  //   replies: [],
+  //   retweets: 0,
+  // },
 ];
