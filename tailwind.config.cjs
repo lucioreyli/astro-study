ff14527706f060520a74f03f0f69a1a39eb9aa5c
Lúcio Andrade <lucioandradejr@gmail.com>
2023-10-02 12:35:06 -0400

    chore: upgrade astro to 3.2.1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: ['class', '[data-theme="dark"]'],
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				background: "var(--background)",
-				gray: "var(--gray)",
-				border: "var(--border)",
-				foreground: "var(--foreground)",
-				primary: "var(--primary)",
-				muted: {
-					foreground: "var(--muted-foreground)",
-					DEFAULT: "var(--muted)"
-				}
-			}
-		},
-	},
-	plugins: [],
-}
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        gray: "var(--gray)",
+        border: "var(--border)",
+        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        muted: {
+          foreground: "var(--muted-foreground)",
+          DEFAULT: "var(--muted)",
+        },
+        accent: {
+          blue: "var(--accent-blue)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
